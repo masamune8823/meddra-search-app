ad098ac59d352b1863d5ba6d48ee88f933a91eb4
masamune8823 <lazysuzan1972@gmail.com>
2025-04-16 21:45:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import os
 import faiss
-QUERY_CACHE_PATH = "data/query_expansion_cache.pkl"
 
 from helper_functions import (
     encode_query,
